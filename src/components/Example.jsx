faa9fba1db43e34430405b736dba8d57f8557068
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-06-10 16:34:56 -0400

    Revert table

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -5,11 +5,7 @@ import { QUERY_USER } from '@/graphql/queries/users'
 
 import {
   Stack,
-  Heading,
-  Table,
-  Thead,
-  Tr,
-  Th
+  Heading
 } from '@chakra-ui/react'
 
 export default function Example () {
@@ -21,26 +17,12 @@ export default function Example () {
 
   const users = data?.users
 
-  console.log(users)
-
   return (
     <Stack mt='8' alignItems='center'>
       {fetching && <p>Loading...</p>}
       {error && <p>{error.message}</p>}
       {users && users.map(user => (
-        <Table
-          key={user.id}
-          variant='simple'
-        >
-          <Thead>
-            <Tr>
-              <Th>Full Name</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
-            </Tr>
-          </Thead>
-          <Heading as='h3' size='sm'>{user.fullName}</Heading>
-        </Table>
+        <Heading key={user.id} as='h3' size='sm'>{user.fullName}</Heading>
       ))}
     </Stack>
   )
